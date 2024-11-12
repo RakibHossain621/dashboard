e1b63d40c0cdfe54d9bd89483ea71976f15dc61e
RakibHossain621 <rakibhasanrasel07@gmail.com>
2024-11-12 19:45:41 +0600

    change

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -2,16 +2,26 @@ import Navbar from '@/component/Navbar'
 import Sidebar from '@/component/Sidebar'
 import React from 'react'
 
-const layout = ({children}) => {
+export default function Layout({ children }) {
   return (
-    <>
-    <div className='flex'>
-    <Sidebar/> 
-     <Navbar/>
-      {children}
-    </div>
-    </>
-  )
-}
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <aside className="w-64  flex flex-col items-center py-4 fixed h-full">
+        <Sidebar />
+      </aside>
 
-export default layout
+      {/* Main content */}
+      <div className="flex flex-col flex-1 ml-64">
+        {/* Navbar */}
+        <header className="h-16 flex items-center justify-between px-6 sticky top-0 z-10">
+          <Navbar />
+        </header>
+
+        {/* Main content area */}
+        <main className="flex-1 p-6 ">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
